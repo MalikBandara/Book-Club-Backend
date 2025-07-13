2d533c43cc98a6dab707698ed508b827a2e0486f
MalikBandara <mmalith520@gmail.com>
2025-07-13 13:11:50 +0530

    Backend create and mongoDb connect

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+// import dotenv without this not working env file
+import dotenv from "dotenv";
+import { ConnectDb } from "./db/db";
+
+//config the dotenv file
+dotenv.config();
+//assign express backend
+const app = express();
+
+const PORT = process.env.SERVER_PORT;
+
+ConnectDb().then(() => {
+  app.listen(PORT || 3000, () => {
+    console.log(`Server is running  ${PORT}`);
+  });
+});
